840d6180688c1df678003f21ea091060950fd7c9
Guilherme Nunes <guilherme.snunes@ibm.com>
2021-09-02 17:23:49 -0300

    Projeto iniciado

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Primeiro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello IBM!");
+            Console.ReadLine();
+        }
+    }
+}
